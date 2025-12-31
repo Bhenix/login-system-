4fd6e48ea6f3c24a3a2c5c3301c5da8ef9262626
Ayim Bhani <ayimbhani@gmail.com>
2025-12-31 17:34:11 +0000

    Version 4: Security Ugrade i hashed the password and strength password

--- loging_system.py
+++ loging_system.py
@@ -17,8 +17,23 @@ while True:
 
     if state == 1:
         user_name = input('Create username: ')
-        user_password = input('Create password: ')
+        while True:
+
+            user_password = input('Create password: ')
+            if len(user_password) <8:
+
+                print('Password too short. Must be atleast 8 characters.')
+                continue
+            if not any(char.isdigit() for char in user_password):
+                print('Password must include at least one number.')
+                continue
+            print('Strong Password')
+            break 
+
         account[user_name] = user_password
+        with open('accounts.txt', 'a') as file:
+            file.write(f'{user_name}:{user_password}\n')
+        
     
 
         print('Account successfully created')
@@ -29,6 +44,7 @@ while True:
         login_pass = input('Enter password: ')
         if user_login in account and account [user_login] == login_pass :
             print(f'{user_login} You are Welcome once aagin\nWe have missed your pressence')
+
         else:
             print('Invalid username or password')
     elif state ==3:
