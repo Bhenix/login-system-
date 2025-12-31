1073d84f2ef1b95dbf30ca99d3bb667cec174d2a
Ayim Bhani <ayimbhani@gmail.com>
2025-12-31 16:52:08 +0000

    Version 3: Looping program and persistent loging system thus, the file is saved

--- loging_system.py
+++ loging_system.py
@@ -1,23 +1,38 @@
 print('Welcome to Eternal web')
 account = {}
-print('Do you want to login or signUp')
-state = int(input('Enter 0 to login and 1 to create account'))
+try:
+    with open('account.txt','r') as file:
+        for line in file:
+            user_name, user_password = line.strip().split(':')
+            account[user_name] = user_password
+except FileNotFoundError:
+    pass 
 
-if state == 1:
-    user_name = input('Create username: ')
-    user_password = input('Create password: ')
-    account[user_name] = user_password
-    account['password'] = user_password
+while True:
+    print(f'\n1. sign Up')
+    print(f'2. Login')
+    print(f'3. Exit')
 
-    print('Account successfully created')
+    state = int(input('Select your choice'))
 
-elif state ==0:
-    print('Login')
-    user_login = input('Enter username: ')
-    login_pass = input('Enter password: ')
-    if user_login in account and account [user_login] == login_pass :
-        print(f'{user_login} You are Welcome once aagin\nWe have missed your pressence')
+    if state == 1:
+        user_name = input('Create username: ')
+        user_password = input('Create password: ')
+        account[user_name] = user_password
+    
+
+        print('Account successfully created')
+
+    elif state ==2:
+        print('Login')
+        user_login = input('Enter username: ')
+        login_pass = input('Enter password: ')
+        if user_login in account and account [user_login] == login_pass :
+            print(f'{user_login} You are Welcome once aagin\nWe have missed your pressence')
+        else:
+            print('Invalid username or password')
+    elif state ==3:
+        print('Goodbye')
+        break 
     else:
-        print('Invalid username or password')
-else:
-    print('Do the right thing ')
+        print('Do the right thing ')
