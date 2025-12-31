eb7e94a29f4024ee517c2f9eff262ce4e4192dca
Ayim Bhani <ayimbhani@gmail.com>
2025-12-31 16:30:42 +0000

    Version 2: Multiple account creation

--- loging_system.py
+++ loging_system.py
@@ -6,13 +6,18 @@ state = int(input('Enter 0 to login and 1 to create account'))
 if state == 1:
     user_name = input('Create username: ')
     user_password = input('Create password: ')
+    account[user_name] = user_password
+    account['password'] = user_password
 
-    print('Account successfrlly created')
+    print('Account successfully created')
 
 elif state ==0:
-    print('Loging')
+    print('Login')
     user_login = input('Enter username: ')
     login_pass = input('Enter password: ')
-    print(f'{user_login} You are Welcome once aagin\nWe have missed your pressence')
+    if user_login in account and account [user_login] == login_pass :
+        print(f'{user_login} You are Welcome once aagin\nWe have missed your pressence')
+    else:
+        print('Invalid username or password')
 else:
     print('Do the right thing ')
